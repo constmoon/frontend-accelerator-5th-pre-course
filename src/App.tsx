5cc95f5462fa0cf82949dcc6e79459785009d13a
constmoon <jiwoo.link@gmail.com>
2026-01-20 15:51:10 +0900

    불필요한 query options 제거

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 1,
       refetchOnWindowFocus: false,
     },
   },
